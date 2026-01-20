46d534130d70dd3d82af559b7097b3c062916dee
Rishabh Singh <rishabhrajput5388@gmail.com>
2026-01-20 17:47:27 +0530

    Update logos to provided assets

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
 
                 <div className="relative z-20 flex items-center text-lg font-medium">
                     {/* <Command className="mr-2 h-6 w-6" /> */}
-                    <img src="/logo.png" alt="Mikromedia" className="mr-2 h-10 w-auto" />
+                    <img src="/logo-white.png" alt="Mikromedia" className="mr-2 h-10 w-auto" />
                     Mikromedia
                 </div>
                 <div className="relative z-20 mt-auto">
